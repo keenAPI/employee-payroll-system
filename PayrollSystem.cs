f0da4e3402bda8221c0fb1d8af0383d87d76e28f
Keenan Fourie <Keenan.Fourie@redacademy.co.za>
2025-02-28 15:40:25 +0200

    created a Payroll System class. Still needs more work.

---
+++ PayrollSystem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Payroll_System
+{
+    class PayrollSystem
+    {
+        private List<Employee> employees = new List<Employee>();
+
+        public void AddEmployee(Employee emp)
+        {
+            employees.Add(emp);
+            Console.WriteLine("Employee added successfully!");
+        }
+
+        public void DisplayEmployees()
+        {
+            Console.WriteLine("\nEmployee List:");
+            foreach (var emp in employees)
+            {
+                emp.DisplayEmployee();
+            }
+        }
+
+        public void ProcessPayroll()
+        {
+            Console.WriteLine("\nProcessing Payroll...");
+            foreach (var emp in employees)
+            {
+                double grossSalery = emp.CalculateGrossSalery();
+                double tax = Payroll.CalculateTax(grossSalery);
+            }
+        }
+
+
+    }
+}
