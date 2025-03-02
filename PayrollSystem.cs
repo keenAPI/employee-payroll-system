76ee4a4911966636b2668d7d64240aae23f77464
Keenan Fourie <fouriekeenan9@gmail.com>
2025-03-02 21:44:06 +0200

    Populated the PayrollSystem and added more methods to the files. Added the user interaction in the Program .cs file

--- PayrollSystem.cs
+++ PayrollSystem.cs
@@ -33,6 +33,13 @@ namespace Employee_Payroll_System
             {
                 double grossSalery = emp.CalculateGrossSalery();
                 double tax = Payroll.CalculateTax(grossSalery);
+                double netSalery = Payroll.CalculateNetSalery(grossSalery);
+
+                Console.WriteLine($"Payroll for {emp.Name}:");
+                Console.WriteLine($"Gross Salery: {grossSalery:C}");
+                Console.WriteLine($"Tax Deducted: {tax:C}");
+                Console.WriteLine($"Net Salery: {netSalery:C}");
+                Console.WriteLine("-----------------------------");
             }
         }
 
--- Program.cs
+++ Program.cs
@@ -4,7 +4,54 @@
     {
         static void Main(string[] args)
         {
-           
+           PayrollSystem payrollSystem = new PayrollSystem();
+
+            while (true)
+            {
+                Console.WriteLine("\nEmployee Payroll System");
+                Console.WriteLine("1. Add Employee");
+                Console.WriteLine("2. Display Employees");
+                Console.WriteLine("3. Process Payroll");
+                Console.WriteLine("4. Exit");
+                Console.Write("Enter your choice: ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.Write("Enter Employee ID: ");
+                        int id = int.Parse(Console.ReadLine());
+
+                        Console.Write("Enter Name: ");
+                        string name = Console.ReadLine();
+
+                        Console.Write("Enter Position: ");
+                        string position = Console.ReadLine();
+
+                        Console.Write("Enter Base Salery: ");
+                        double salery = double.Parse(Console.ReadLine());
+
+                        Employee emp = new Employee(id, name, position, salery);
+                        payrollSystem.AddEmployee(emp);
+                        break;
+
+                    case "2":
+                        payrollSystem.DisplayEmployees();
+                        break;
+
+                    case "3":
+                        payrollSystem.ProcessPayroll();
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Exiting...");
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid choice! Please try again.");
+                        break;
+                }
+            }
         }
     }
 }
